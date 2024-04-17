a124e60f5f1582c2e521ccbc105824f4cd30e1de
BenSShin <ben.shin27@gmail.com>
2024-04-17 13:03:13 -0500

    added post for prop

---
+++ routes/api/controllers/propController.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const handleError = require("../../../utils/handleError");
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+const { Prop, PropImage } = require("../../../models/prop");
+
+router.post("/create/prop", upload.single("image"), async (req, res) => {
+  const imageFile = req.file;
+  if (!imageFile) {
+    return res.status(400).json({ error: "No image uploaded." });
+  }
+  try {
+    const propImage = new PropImage({
+      data: imageFile.buffer,
+      contentType: imageFile.mimetype,
+    });
+    await propImage.save();
+
+    const newProp = new Prop({
+      propName: req.body.propName,
+      propImage: propImage._id,
+    });
+    await newProp.save();
+
+    res.status(201).json({ message: "Prop created with image.", prop: newProp });
+  } catch (error) {
+    console.log(error);
+    handleError(error, res);
+  }
+});
+
+// router.get("/:propId/image", async)
