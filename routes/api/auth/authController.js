57a3cc2b7bc8ab8bdeefc56f60a7cadbd827f194
BenSShin <ben.shin27@gmail.com>
2024-05-20 11:21:17 -0500

    added login post request

--- routes/api/auth/authController.js
+++ routes/api/auth/authController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const handleError = require("../../../utils/handleError");
 const User = require("../../../models/user");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   try {
@@ -19,5 +20,27 @@ router.post("/register", async (req, res) => {
   } catch (error) {
     console.log(error);
     handleError(error, res);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid email or passowrd" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, "secretKey");
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log("Error logging in:", error);
+    handleError(erros, res);
   }
 });
